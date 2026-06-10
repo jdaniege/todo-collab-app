2f645bd0d4bfc6cd7f1b82776fb9b13622d2652b
Daniel <jesusdanielv16@gmail.com>
2026-06-10 11:15:36 -0500

    feat: agregar lista de tareas con useState

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,66 @@
-import Image from "next/image";
+"use client"
+import { useState } from "react"
 
 export default function Home() {
-  return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
-  );
+  const [tareas, setTareas] = useState([
+    "Aprender Next.js",
+    "Crear mi primera App",
+    "Subir a Github"
+  ])
+
+  const [nuevaTarea, setNuevaTarea] = useState("")
+
+  const agregarTarea = () => {
+    if (nuevaTarea.trim() === "") return
+    setTareas([...tareas, nuevaTarea])
+    setNuevaTarea("")
+  }
+  const eliminarTarea = (index: number) => {
+    setTareas(tareas.filter((_, i) => i !== index)) 
+  }
+
+  
+return (
+  <main className="min-h-screen p-8 max-w-xl mx-auto">
+    <h1 className="text-3xl font-bold">Mi To-Do App</h1>
+    <p className="text-gray-500 mt-2 mb-8">Lista de tareas colaborativas</p>
+  
+  {/* Input para agregar las tareas */}
+  <div className="flex gap-2 mb-6">
+    <input
+      type="text"
+      value={nuevaTarea}
+      onChange={(e) => setNuevaTarea(e.target.value)}
+      placeholder="Escribe una tarea..."
+      className="flex-1 p-3 border rounded-lg" />
+ 
+    <button onClick={agregarTarea}
+    className="px-4 py-3 bg-blue-500 text-white rounded-lg hover:bg-blue-600">
+
+      Agregar
+    </button>
+  </div>
+  
+  {/* Input para agregar las tareas */}
+
+  <ul className="space-y-3">
+    {tareas.map((tarea, index) => (
+      <li key={index} className="p-4 bg-white border rounded-lg shadow-sm text-gray-300 flex justify-between items-center">
+        {tarea}
+        <button  onClick={() => eliminarTarea(index)}
+          className="text-red-400 hover:text-red-600 text-sm">
+         Eliminar
+        </button>
+        </li>
+
+    ))}
+
+    
+
+
+  </ul>
+  </main>
+)
 }
+
+ 
